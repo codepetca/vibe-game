e75d56edda3b057f64bb6d62dd24b23665f25f5c
stew.chan@gmail.com <stew.chan@gmail.com>
2025-04-13 18:26:04 -0400

    begin asteroids game

---
+++ games/asteroids/src/js/game.js
@@ -0,0 +1,16 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [BootScene, PreloadScene, GameScene]
+};
+
+const game = new Phaser.Game(config); 
